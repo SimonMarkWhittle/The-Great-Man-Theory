5bdf3430ba45e96f8797ed68ce98962f03fc7e83
SimonMarkWhittle <smw2ed@virginia.edu>
2017-12-07 17:22:18 -0500

    dude has pike; can swing and shit

--- The Great Man Theory/Assets/Scripts/FollowMouse.cs
+++ The Great Man Theory/Assets/Scripts/FollowMouse.cs
@@ -7,18 +7,23 @@ public class FollowMouse : MonoBehaviour {
     Vector2 anchorOffset = new Vector2();
     Rigidbody2D body;
     GameManager gm;
+    public bool active=true;
 
 	// Use this for initialization
 	void Start () {
         body = GetComponentInParent<Rigidbody2D>();
         anchorOffset += new Vector2(0f, 1f);
         gm = FindObjectOfType<GameManager>();
-        //gm = GameManager.Get();
-        //Debug.Log("FollowMouse gm is: " + gm);
     }
-	
+
 	// Update is called once per frame
 	void Update () {
+        if (active)
+            Forces();
+	}
+
+    void Forces() {
+        Debug.Log("Geddit" + ManagerGetter.Get());
         Camera cam = gm.mainCamera;
         Vector3 mousePos3 = cam.ScreenToWorldPoint(Input.mousePosition);
         Vector2 mousePos = new Vector2(mousePos3.x, mousePos3.y);
@@ -27,7 +32,6 @@ public class FollowMouse : MonoBehaviour {
         Vector2 force = (mousePos - forcePoint) * gm.extraForce;
         // force.Normalize();
         // force *= gm.maxForce;
-
         body.AddForceAtPosition(force, forcePoint);
-	}
+    }
 }
--- The Great Man Theory/Assets/Scripts/GameManager.cs
+++ The Great Man Theory/Assets/Scripts/GameManager.cs
@@ -4,30 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-    private static GameManager gm;
     public Camera mainCamera;
     public float extraForce;
     public float maxForce;
 
 	// Use this for initialization
 	void Start () {
-        gm = this;
+        ManagerGetter.gm = this;
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+}
+
+public static class ManagerGetter {
+    public static GameManager gm;
 
     public static GameManager Get() {
-        Debug.Log(gm);
-        Debug.Log("Camera is: " + gm.mainCamera);
-        if (gm = null) {
-            Debug.Log("Wanna getta gm");
-            gm = MonoBehaviour.FindObjectOfType<GameManager>();
-            Debug.Log("gm is now: " + gm);
-        }
         return gm;
     }
-
 }
