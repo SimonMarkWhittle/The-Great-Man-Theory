8f11461801fdd39f2625bee9cdf94737700bca65
SimonMarkWhittle <smw2ed@virginia.edu>
2017-12-08 13:34:51 -0500

    head tracking now works

--- The Great Man Theory/Assets/Scripts/HeadTracking.cs
+++ The Great Man Theory/Assets/Scripts/HeadTracking.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 public class HeadTracking : MonoBehaviour {
 
     GameManager gm;
-    public bool active = true;
     public float maxDegrees = 1000f;
     public GameObject point;
 
@@ -16,38 +15,12 @@ public class HeadTracking : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        if (active)
-            Look();
+        Look();
 	}
 
     void Look() {
-        Camera cam = gm.mainCamera;
-        Vector2 pos = gameObject.transform.position;
-        // Vector2 mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-        Vector2 mousePos = point.transform.position;
-        Vector2 diffPoint = mousePos - pos;
-
-        Vector2 forward = gameObject.transform.up;
-
-        float angle = AngleBetweenVector2(forward, diffPoint);
-        Quaternion lookRot = Quaternion.Euler(0f, 0f, angle);
-        Debug.Log(lookRot.eulerAngles);
-
-        gameObject.transform.rotation = Quaternion.RotateTowards(gameObject.transform.rotation, lookRot, 90f);
+        gameObject.transform.LookAt(point.transform, Vector3.forward);
+        Vector3 angles = gameObject.transform.rotation.eulerAngles;
+        gameObject.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, angles.z));
     }
-
-    static float AngleBetweenVector2(Vector2 vec1, Vector2 vec2) {
-        Vector2 vec1Rotated90 = new Vector2(-vec1.y, vec1.x);
-        float sign = (Vector2.Dot(vec1Rotated90, vec2) < 0) ? -1.0f : 1.0f;
-        return Vector2.Angle(vec1, vec2) * sign;
-    }
-
-    /*
-function eeulervector(euler:Vector3):Vector3;//convert euler to vector3.
-{
-float elevation = Deg2Rad(euler. x);
-float heading = Deg2Rad(euler. y)
-return Vector3(Cos(elevation) * Sin(heading), Sin(elevation), Cos(elevation) * Cos(heading));
-}
-    */
 }
