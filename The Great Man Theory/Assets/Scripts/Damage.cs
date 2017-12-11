be115726a916cb2bde0fdea23adbc6d78c0ec106
SimonMarkWhittle <smw2ed@virginia.edu>
2017-12-10 21:13:38 -0500

    fixed some collision stuff

--- The Great Man Theory/Assets/Scripts/Damage.cs
+++ The Great Man Theory/Assets/Scripts/Damage.cs
@@ -6,9 +6,15 @@ public class Damage : MonoBehaviour {
 
     public float health = 100f;
 
+    public GameObject weapon;
+
 	// Use this for initialization
 	void Start () {
-		
+        if (weapon) {
+            Collider2D weaponCollider = weapon.GetComponent<Collider2D>();
+            Collider2D thisCollider = gameObject.GetComponent<Collider2D>();
+            Physics2D.IgnoreCollision(weaponCollider, thisCollider);
+        }
 	}
 	
 	// Update is called once per frame
